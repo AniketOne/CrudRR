377240073cb43f9a8f1d588c8f7f3908d67eb6d1
Aniket Singh <aniket@onehash.ai>
2023-01-02 12:25:22 +0530

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.17.1"
 set :application, 'ex'
 set :repo_url, 'git@github.com:AniketOne/CrudRR.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/'
+set :deploy_to, '/home/deploy/ex'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
